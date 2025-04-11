d47f208781529790fc8b65d9b96521455e369082
Timur Usmanov <fonuatimur@gmail.com>
2025-04-11 18:34:18 +0500

    flake8 alignment

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -10,5 +10,7 @@ class Reservation(Base):
     id = Column(Integer, primary_key=True, index=True)
     customer_name = Column(String, nullable=False)
     table_id = Column(Integer, ForeignKey('tables.id'), nullable=False)
-    reservation_time = Column(SQLAlchemyDateTime(timezone=True), nullable=False)
+    reservation_time = Column(
+        SQLAlchemyDateTime(timezone=True), nullable=False
+    )
     duration_minutes = Column(Integer, nullable=False)
